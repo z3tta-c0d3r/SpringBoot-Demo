a34d7c3cac138b888126427aeb5e26fffc7b3c81
Zeta <vicente.garcia@outlook.com>
2019-12-09 20:39:04 +0100

    fixed  Spring Boot OAuth 2 Client Credentials Grant

---
+++ src/main/java/com/example/SpringDemo2/AuthorizationServer.java
@@ -0,0 +1,34 @@
+package com.example.SpringDemo2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServer extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    @Qualifier("authenticationManagerBean")
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                .inMemory()
+                .withClient("javainuse-client")
+                .secret("{noop}javainuse-secret")
+                .authorizedGrantTypes("client_credentials")
+                .scopes("resource-server-read", "resource-server-write");
+    }
+}
--- src/main/java/com/example/SpringDemo2/SpringDemo2Application.java
+++ src/main/java/com/example/SpringDemo2/SpringDemo2Application.java
@@ -4,14 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
+@RestController
+@EnableResourceServer
 @SpringBootApplication
-@EnableAuthorizationServer
 public class SpringDemo2Application {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(SpringDemo2Application.class, args);
+	}
+
+	//This method will be used to check if the user has a valid token to access the resource
+	@RequestMapping("/validateUser")
+	public Principal user(Principal user) {
+		return user;
 	}
 
 }
