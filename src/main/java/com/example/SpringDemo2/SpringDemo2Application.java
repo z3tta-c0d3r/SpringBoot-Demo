adfd5c7772b0e4288fc2affe80d11a10138a7240
n65202 <vicente.garcia@aquanima.com>
2019-12-09 19:55:28 +0100

    fixes

--- src/main/java/com/example/SpringDemo2/SpringDemo2Application.java
+++ src/main/java/com/example/SpringDemo2/SpringDemo2Application.java
@@ -2,11 +2,15 @@ package com.example.SpringDemo2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
+@EnableAuthorizationServer
 public class SpringDemo2Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringDemo2Application.class, args);
 	}
 
